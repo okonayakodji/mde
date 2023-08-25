d5694d970b9be23ef8627d457f3c5b7fa2b7dd05
okonayakodji <okonayakodji@mail.ru>
2023-08-25 20:59:20 +0300

    Add menu preset

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,7 +10,19 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    // The File Submenu
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let close = CustomMenuItem::new("close".to_string(), "Close");
+    let file_menu = Menu::new()
+        .add_item(quit)
+        .add_item(close);
+    let file = Submenu::new("File", file_menu);
+
+    let menu = Menu::new()
+        .add_native_item(MenuItem::Copy)
+        .add_submenu(file);
     tauri::Builder::default()
+        .menu(menu)
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
